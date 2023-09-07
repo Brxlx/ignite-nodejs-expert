348ad75f237ba25a114868dc946affe02000d131
Bruno Santos <brcesar19@gmail.com>
2023-09-07 16:16:00 -0300

    chore: value object for slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,7 +1,9 @@
 import { randomUUID } from 'node:crypto';
+import { Slug } from './value-objects/slug';
 interface QuestionProps {
   title: string;
   content: string;
+  slug: Slug;
   authorId: string;
 }
 
@@ -9,11 +11,13 @@ export class Question {
   public id: string;
   public title: string;
   public content: string;
+  public slug: Slug;
   public authorId: string;
 
   constructor(props: QuestionProps, id?: string) {
     this.title = props.title;
     this.content = props.content;
+    this.slug = props.slug;
     this.authorId = props.authorId;
     this.id = id ?? randomUUID();
   }
