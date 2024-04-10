3b1431a075ac1d573c18bffb039e429044a0367c
Bruno Santos <brcesar19@gmail.com>
2024-04-10 17:48:32 -0300

    feat(answer): add use case, repository, inMemory, factory

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -5,8 +5,25 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer';
 export class InMemoryAnswersRepository implements AnswersRepository {
   // public items: Answer[] = [];
   public items = new Map<UniqueEntityID, Answer>();
+
+  async findById(id: string): Promise<Answer | null> {
+    const answer = Array.from(this.items.values()).find(item => item.id.toString() === id);
+
+    if (!answer) return null;
+
+    return answer;
+  }
+
   async create(answer: Answer): Promise<void> {
     // this.items.push(answer);
     this.items.set(answer.id, answer);
+  }
+
+  async delete(answer: Answer): Promise<void> {
+    const answerExists = Array.from(this.items.values()).find(item => item.id === answer.id);
+
+    if (answerExists) {
+      this.items.delete(answerExists.id);
+    }
   }
 }
