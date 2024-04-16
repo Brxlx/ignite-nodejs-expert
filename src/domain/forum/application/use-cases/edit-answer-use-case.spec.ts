19b59a4ec7cb2c464ca81a46cb9cd833f8792616
Bruno Santos <brcesar19@gmail.com>
2024-04-16 20:14:37 -0300

    chore: change errors to core; update imports; add read notification use case

--- src/domain/forum/application/use-cases/edit-answer-use-case.spec.ts
+++ src/domain/forum/application/use-cases/edit-answer-use-case.spec.ts
@@ -2,8 +2,8 @@ import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-r
 import { makeAnswer } from 'test/factories/make-answer';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { EditAnswerUseCase } from './edit-answer-use-case';
-import { NotAllowedError } from './errors/not-allowed-error';
-import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import { NotAllowedError } from '@/core/errors/not-allowed-error';
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error';
 import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository';
 import { makeAnswerAttachment } from 'test/factories/makeAnswerAttachment';
 
