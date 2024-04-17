f976c19d1064f7d8ac81c71191f5a7982f10cc6e
Bruno Santos <brcesar19@gmail.com>
2024-04-17 13:59:17 -0300

    feat(answer): add domain event; dispatch event

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -1,7 +1,8 @@
-import { Entity } from '@/core/entities/entity';
 import { Optional } from '@/core/types/optional';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { AnswerAttachmentList } from './answer-attachment-list';
+import { AggregateRoot } from '@/core/entities/aggregate-root';
+import { AnswerCreatedEvent } from '../events/answer-created-event';
 
 export interface AnswerProps {
   authorId: UniqueEntityID;
@@ -11,7 +12,7 @@ export interface AnswerProps {
   createdAt: Date;
   updatedAt?: Date;
 }
-export class Answer extends Entity<AnswerProps> {
+export class Answer extends AggregateRoot<AnswerProps> {
   get authorId() {
     return this.props.authorId;
   }
@@ -55,7 +56,7 @@ export class Answer extends Entity<AnswerProps> {
   }
 
   static create(props: Optional<AnswerProps, 'createdAt' | 'attachments'>, id?: UniqueEntityID) {
-    return new Answer(
+    const answer = new Answer(
       {
         ...props,
         attachments: props.attachments ?? new AnswerAttachmentList(),
@@ -63,5 +64,13 @@ export class Answer extends Entity<AnswerProps> {
       },
       id
     );
+
+    const isNewAnswer = !id;
+
+    if (isNewAnswer) {
+      answer.addDomainEvent(new AnswerCreatedEvent(answer));
+    }
+
+    return answer;
   }
 }
